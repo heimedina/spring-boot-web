1615b58b7088c5bb278ce0e4097b2ba9a1b3da7c
heimedina <heidimedina23@gmail.com>
2025-04-15 11:23:11 +0200

    Subo proyecto web

---
+++ src/main/java/com/ideas/springboot/web/app/SringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.ideas.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringBootWebApplication.class, args);
+	}
+
+}
