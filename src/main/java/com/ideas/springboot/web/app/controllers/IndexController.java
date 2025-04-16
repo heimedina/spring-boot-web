418e318ad5ee22665f526cb133f658f9cb65ee40
heimedina <heidimedina23@gmail.com>
2025-04-16 10:44:19 +0200

    controllers y params

--- src/main/java/com/ideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/ideas/springboot/web/app/controllers/IndexController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ideas.springboot.web.app.models.Usuario;
@@ -36,13 +37,19 @@ public class IndexController {
 	
 	@RequestMapping("/listar")
 	public String listar(Model model) {
+		
+		model.addAttribute("titulo", "Listado de usuarios");
+		
+		return "listar";
+		
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
 		List<Usuario> usuarios = Arrays.asList(new Usuario("Luis","Perez","correo1@gmail.com"),
 				new Usuario("Andres","Guzman","correo2@gmail.com"),new Usuario("Raul","Gonzalez","correo3@gmail.com"),
 				new Usuario("Pablo","Medina","pablo3@gmail.com"));
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
-		
-		return "listar";
+		return usuarios;
 		
 	}
 
